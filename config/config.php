81fbb388aebd079605730d302d8ef05133d45566
Lalo <66379947+N3ckz0@users.noreply.github.com>
2024-02-07 20:19:01 -0600

    Add files via upload

---
+++ config/config.php
@@ -0,0 +1,10 @@
+<?php
+	define("SERVER", "localhost");
+	define("USER", "root");
+	define("PASSWORD", "");
+	define("DATABASE", "");
+
+	define("CONTROLADOR_PRINCIPAL", "");
+	define("ACCION_PRINCIPAL", "");
+
+?>
